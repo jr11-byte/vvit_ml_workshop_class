4c6d1fcc33e8c40efe8fcb18659af8e6e3012031
jr11-byte <bpjr1104@gmail.com>
2026-03-18 12:05:28 +0530

    Update ml_taxi_app.py

--- ml_taxi_app.py
+++ ml_taxi_app.py
@@ -24,5 +24,39 @@ df['fare'] = pd.to_numerical(df['fare'], errors='coerce')
 x = df[['distance']]
 y = df['fare']
 
-X_train, X_test, y_train, y_test = train_test_split{
-   X
+X_train, X_test, y_train, y_test = train_test_split(
+   X, y, test_size=0.2, random_state=42
+)
+
+model = LinearRegression()
+model.fit(x_train,y_train)
+
+y_pred = model.predict(x_test)
+
+r2 = r2_score(y_test,y_pred)
+rmse = np.sqrt(mean_squared_errorly(y_test,y_pred))
+
+st.subheader("Model Performance")
+st.write(f"R2 Score: {r2:.2f}")
+st.write(f"RMSE: {rmse:.2f}")
+st.subheader("Enter Trip Details")
+distance = st.number_input(
+  "step 1:Enter Distance(km)",
+  min_value=0.0,
+  values=5.0
+)
+passengers = st.number_input(
+  "step 2: Number of passengers",
+  min_value=1,
+  value=1
+)
+hour = st.number_input(
+  "step 3:Hour of Day(0-23)",
+  min_value=0,
+  max_value=23,
+  value=12
+)
+if st.button(" Predict Fare "):
+  input_data = np.array([[distance]])
+  prediction model.predict(input_data)
+  st.success(f" Estimated Fare: ${prediction[0]:.2f}")
